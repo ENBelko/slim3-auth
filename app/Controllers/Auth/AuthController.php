597ee1d85b1838b7d46aee9910ede5539141acb2
ENBelko <jekkiscript@gmail.com>
2018-11-06 14:45:33 +0600

    sign in added

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -8,6 +8,33 @@ use Respect\Validation\Validator as v;
 
 class AuthController extends Controller
 {
+    public function getSignIn($request, $response)
+    {
+        return $this->view->render($response, 'auth/signin.twig');
+    }
+
+    public function postSignIn($request, $response)
+    {
+        $validation = $this->validator->validate($request, [
+            'email' => v::noWhitespace()->notEmpty()->email(),
+            'password' => v::noWhitespace()->notEmpty(),
+        ]);
+
+        if ($validation->failed()) {
+            return $response->withRedirect($this->router->pathFor('auth.signin'));
+        }
+
+        $auth = $this->auth->attempt(
+            $request->getParam('email'),
+            $request->getParam('password')
+        );
+
+        if (!$auth) {
+            return $response->withRedirect($this->router->pathFor('auth.signin'));
+        }
+
+        return $response->withRedirect($this->router->pathFor('home'));
+    }
 
     public function getSignUp($request, $response)
     {
@@ -33,6 +60,11 @@ class AuthController extends Controller
             'name' => $request->getParam('name'),
             'password' => password_hash($request->getParam('password'), PASSWORD_DEFAULT)
         ]);
+
+        $this->auth->attempt(
+            $user->email,
+            $request->getParam('password')
+        );
 
         return $response->withRedirect($this->router->pathFor('home'));
     }
