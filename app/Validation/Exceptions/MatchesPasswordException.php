dfca946bb350c5144808f4f383b263d183dec4ce
ENBelko <jekkiscript@gmail.com>
2018-11-06 17:10:03 +0600

    password changing added

---
+++ app/Validation/Exceptions/MatchesPasswordException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ENBElko
+ * Date: 06.11.2018
+ * Time: 16:58
+ */
+
+namespace App\Validation\Exceptions;
+
+
+class MatchesPasswordException
+{
+
+}
---
+++ app/Validation/Rules/MatchesPassword.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ENBElko
+ * Date: 06.11.2018
+ * Time: 16:53
+ */
+
+namespace App\Validation\Rules;
+
+
+class MatchesPassword
+{
+
+}
